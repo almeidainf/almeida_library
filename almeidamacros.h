535776898b359172e0fae4b35e4a850fb06d7676
Tiago de Almeida <tiago@almeida.inf.br>
2013-03-20 04:17:26 -0300

    Macro for verbose messages.

--- almeidamacros.h
+++ almeidamacros.h
@@ -4,9 +4,16 @@
  * Library of useful macros.
  */
 
-/* Used to error messages. Sends the message to stderr. */
+/* Used for error messages. Sends the message to stderr. */
 #define error(msg, ...)\
 do{\
 	fprintf(stderr, "Error: " msg "\n", ##__VA_ARGS__);\
 	fflush(stderr);\
+} while(0)
+
+/* Macro used for verbose messages. Sends the message to stdout. */
+#define verbose(msg, ...)\
+do{\
+	fprintf(stdout, " - " msg "\n", ##__VA_ARGS__);\
+	fflush(stdout);\
 } while(0)
